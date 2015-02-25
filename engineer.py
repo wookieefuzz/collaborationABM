0a17832ed479cf4d3485b7fd9ff838bbc76f98aa
wookieefuzz <wookie.fuzz@gmail.com>
2015-02-25 02:37:48 -0500

    started work on engineer class

---
+++ engineer.py
@@ -0,0 +1,24 @@
+class engineer:
+    
+    # engineers are spawned with a number of tasks
+    # they run models which will tell them what direction to head
+    # their models are dependent on other users progress
+    # if another user is generating bad information but is unaware of it, design will take longer to close
+    # tasks have varying levels of priority
+    # impact of bad decision on low priority task is small
+    # each task has an associated model
+    
+    
+    
+    def __init__(self,name,numTasks):
+        self.name = name
+        self.numTasks = numTasks
+        
+        
+    def work(self,timeStep):
+        a = 1
+        
+        
+    def runModel(self):
+        a = 1
+    
